211a2dbf7a6ffc21024bdab52d2ea989846a6749
Mehmet Sefa BALIK <sefa.balik@bitaksi.com>
2018-05-28 22:36:32 +0300

    project structure is done

---
+++ UI/index.js
@@ -0,0 +1,75 @@
+
+// Import express and request modules
+var express = require('express');
+var request = require('request');
+
+
+var clientId = 'secret';
+var clientSecret = 'secret';
+
+// Instantiates Express and assigns our app variable to it
+var app = express();
+
+
+// Again, we define a port we want to listen to
+const PORT=5044;
+
+var val;
+
+// Lets start our server
+app.listen(PORT, function () {
+    console.log("Example app listening on port " + PORT);
+});
+
+
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) {
+    res.send('live Ngrok is working! Path Hit: ' + req.url);
+});
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        // If it's there...
+
+        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+        request({
+            url: 'https://slack.com/api/oauth.access', //URL to hit
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+            method: 'GET', //Specify the method
+
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+
+            }
+        })
+    }
+});
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+app.post('/command/:id', function(req, res) {
+    val = req.params.id;
+    res.send(val + ' yolladiniz');
+});
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+app.post('/getval', function(req, res) {
+    res.send('Value: '+ val);
+});
+
+app.post('/command', function(req, res) {
+    if(val == 1)
+        res.send('Yemekhane sırası boş! Çabuk ol!');
+    else if(val == 2)
+        res.send('Yemekhane sırası yarı dolu.');
+    else
+        res.send('Yemekhane sırası dolu :(');
+});
